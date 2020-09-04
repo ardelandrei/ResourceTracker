8a24ede1892438630d9b712e7378f73115ac27db
Andrei Ardel <ardel_andrei@yahoo.com>
2020-09-04 12:36:08 +0300

    basic tests

---
+++ ResourceTracker/Shared/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourceTracker.Shared.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<Label> Labels { get; set; }
+    }
+}
